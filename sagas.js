f9a136d79f1560edd93da7db5af2304c4e8321cb
nelv13 <jayhspark@gmail.com>
2021-02-17 18:23:42 +0900

    finish testing code

--- sagas.js
+++ sagas.js
@@ -1,3 +1,25 @@
+import { put, takeEvery, all } from "redux-saga/effects"
+
+
+const delay = (ms) => new Promise(res => setTimeout(res, ms))
+
 export function* helloSaga() {
   console.log('hello sagas!')
+}
+
+export function* incrementAsync(){
+  yield delay(1000)
+  yield put({ type:'INCREMNET' })
+}
+
+export function* watchIncrementAsync() {
+
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+export default function* rootSaga() {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ])
 }
