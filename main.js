1b889dc3e5802b9bed963c187c9d9dc6e8bc6f48
nelv13 <jayhspark@gmail.com>
2021-02-17 16:25:49 +0900

    create project tutorial

---
+++ main.js
@@ -0,0 +1,26 @@
+import "babel-polyfill"
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+
+import Counter from './Counter'
+import reducer from './reducers'
+import { helloSaga } from './sagas'
+ 
+const store = createStore(reducer)
+
+const action = type => store.dispatch({type})
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => action('INCREMENT')}
+      onDecrement={() => action('DECREMENT')} />,
+    document.getElementById('root')
+  )
+}
+
+render()
+store.subscribe(render)
